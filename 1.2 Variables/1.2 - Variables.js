3255aba359074660b19a56c78a464cfab0e5b110
jebu0 <jesse.burke-black@stu.ocsb.ca>
2024-02-25 15:53:51 -0500

    inital commit

---
+++ 1.2 Variables/1.2 - Variables.js
@@ -0,0 +1,12 @@
+// Part 1
+let length = 10
+let width = 5
+let area = length * width
+console.log(area)
+area = "Area: " + length * width
+console.log(area)
+// Part 2 
+let first_name = "Santa"
+let last_name = "Claws"
+last_name = "Claus"
+console.log("I still believe in", first_name, last_name + ".")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unit-1
+All files for unit 1 will be uploaded here
