10f6e7bcb7239b505b191c52765b48622a9cc03c
jebu0 <jesse.burke-black@stu.ocsb.ca>
2024-02-25 16:40:19 -0500

    Completed work

---
+++ 1.3 Mathematics/1.3 Mathematics.js
@@ -0,0 +1,19 @@
+// Part 1 
+console.log("Part 1:")
+let celsius = 100
+let farhrenheit = 0 
+celsius = celsius + 2
+farhrenheit = (celsius * (9/5)) + 32
+console.log(celsius, "degrees Celsius is", farhrenheit, "degrees Farhrenheit.")
+// Part 2
+console.log("Part 2:")
+farhrenheit = 77
+celsius = (farhrenheit - 32) / (9/5) 
+console.log(farhrenheit, "degrees Farhrenheit is", celsius , "degrees Celsius.")
+// Part 4 
+let x1 = 0
+let x2 = 6
+let y1 = 5
+let y2 = -4
+let m = (y2 - y1)/(x2 - x1)
+console.log(m)
